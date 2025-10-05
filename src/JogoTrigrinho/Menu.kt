e9b1f6620ba2d32a42cd765466670047ed7d577c
Valakzz <chucklord14@gmail.com>
2025-10-05 15:32:12 -0300

    Adicionado Minigame Pedra, Papel, Tesoura

--- src/JogoTrigrinho/Menu.kt
+++ src/JogoTrigrinho/Menu.kt
@@ -2,20 +2,18 @@ package JogoTrigrinho
 
 fun main() {
 
-
-
     val simbolos = listOf("🍒", "🍋", "🍇", "🔔", "⭐", "💎")
     val tigrinho = MenuPrincipal( simbolos)
     tigrinho.TelaPrincipal()
 
+
 }
 class MenuPrincipal(  val simbolos: List<String>) {
-
-    val depositos = Depositos(0)
+    var gamepapel = MinigamePapelPedraTesoura()
     fun Simboloscassino(){
+        var deposit = MinigameTigrinho()
 
-        if (depositos.valores <= 0){
-
+        if (MinigameTigrinho.valores <= 0){
             println("Você não depositou um valor valido")
         }else{
             val  primeirosimbolo = simbolos.random()
@@ -24,42 +22,38 @@ class MenuPrincipal(  val simbolos: List<String>) {
             println(" Rodada ${primeirosimbolo} : ${Segundosimbolo} : ${Terceirosimbolo}")
             if (primeirosimbolo == Segundosimbolo && Segundosimbolo == Terceirosimbolo){
                 println("Parabéns você GANHOU parceiro \uD83D\uDC2F\uD83D\uDC2F")
-                depositos.valores += 5000
+                MinigameTigrinho.valores += 5000
             }else{
                 println("Não foi dessa vez tente novamente \uD83D\uDC38")
-                depositos.valores -= 50
-
+                MinigameTigrinho.valores -= 50
             }
-
         }
-
-
     }
 
     fun TelaPrincipal(){
-
         var opcao: Int
-
+        var deposit = MinigameTigrinho()
         do {
             println("-------------------------------------------------------")
             println(" --- Bem vindo ao cassino dos baianos \uD83C\uDDEA\uD83C\uDDE8 --- ")
             println("1. Digite o valor que deseja depositar ");
-            println("2. Começar o jogo \uD83C\uDF40 ");
-            println("3. Gostaria de Olhar seu Deposito ");
+            println("2. Minigame Tigrinho \uD83C\uDF40 ");
+            println("3. Minigame Pedra, Papel Tesoura ");
+            println("4. Gostaria de Olhar seu Deposito ")
 
             println("0. Sair \uD83D\uDC38");
             print("Digite uma opcao: \uD83D\uDC40 ");
             opcao = readln().toInt();
 
             when(opcao){
-                2 -> Simboloscassino()
                 0 -> println(" Você está saindo do Cassino \uD83D\uDE43 \uD83D\uDE43 ")
-
                 1 ->{
                     println("Digite o valor que deseja depositar")
-                    depositos.Depositados(readln().toInt())
+                   deposit.Depositados(readln().toInt())
                 }
-                3 -> println("Olá seu valor depositado é ${depositos.valores}")
+                2 -> Simboloscassino()
+                3 -> gamepapel.JogarPapelPedraTesoura()
+                4 -> println("Olá seu valor depositado é ${MinigameTigrinho.valores}")
             }
         }while (opcao != 0)
     }
--- src/JogoTrigrinho/MinigameTigrinho.kt
+++ src/JogoTrigrinho/MinigameTigrinho.kt
@@ -1,7 +1,13 @@
 package JogoTrigrinho
 
-class Depositos( var valores: Int) {
-    fun Depositados(Novovalor: Int){
+ open class MinigameTigrinho() {
+
+
+    companion object{
+        var valores: Int = 0
+
+    }
+    fun Depositados(Novovalor: Int = 0){
         if (Novovalor <= 0){
             println("O valor que você Depositou é invalido")
         }else{
